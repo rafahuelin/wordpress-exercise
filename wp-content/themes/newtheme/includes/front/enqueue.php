052fd8a395ddafe8a963fc99ec7b19673032a43b
Rafa Huelin <it@engisa.com>
2017-08-31 20:37:39 +0100

    enqueue styles

--- wp-content/themes/newtheme/includes/front/enqueue.php
+++ wp-content/themes/newtheme/includes/front/enqueue.php
@@ -10,4 +10,15 @@ function nt_enqueue() {
 	wp_register_style( 'nt_magnific-popup', get_template_directory_uri() . '/assets/css/magnific-popup.css' );
 	wp_register_style( 'nt_responsive', get_template_directory_uri() . '/assets/css/responsive.css' );
 	wp_register_style( 'nt_custom', get_template_directory_uri() . '/assets/css/custom.css' );
+
+	wp_enqueue_style( 'nt_google_fonts' );
+	wp_enqueue_style( 'nt_bootstrap' );
+	wp_enqueue_style( 'nt_style' );
+	wp_enqueue_style( 'nt_dark' );
+	wp_enqueue_style( 'nt_font_icons' );
+	wp_enqueue_style( 'nt_animate' );
+	wp_enqueue_style( 'nt_magnific-popup' );
+	wp_enqueue_style( 'nt_responsive' );
+	wp_enqueue_style( 'nt_custom' );
 }
+
